f9b38d28c298b400c26240cc39d213c4b20b54dc
zhangxin <812603872@qq.com>
2024-10-01 21:40:32 +0800

    fix

--- server/handle.js
+++ server/handle.js
@@ -11,10 +11,23 @@ const handleStatic = async (request, response) => {
     if (request.method !== 'get') {
         return true;
     }
+    const extList = [
+        { ext: '.js', type: 'text/javascript;charset=UTF-8', isBase64Encoded: false },
+        { ext: '.html', type: 'text/html;charset=UTF-8', isBase64Encoded: false },
+        { ext: '.css', type: 'text/css;charset=UTF-8', isBase64Encoded: false },
+        { ext: '.mp3', type: 'audio/mpeg', isBase64Encoded: true },
+        { ext: '.jpg', type: 'image/jpeg', isBase64Encoded: true },
+        { ext: '.png', type: 'image/png', isBase64Encoded: true },
+        { ext: '.ico', type: 'image/x-icon', isBase64Encoded: true },
+        { ext: '.woff', type: 'application/font-woff', isBase64Encoded: true },
+        { ext: '.ttf', type: 'font/ttf', isBase64Encoded: true },
+    ];
     const filePath = path_1.default.resolve(__dirname, './' + rawPath);
     const body = (0, fs_1.readFileSync)(filePath, { encoding: 'utf-8' });
     response.statusCode = 200;
-    response.headers = {};
+    response.headers = {
+        'content-type': extList.find(ext => rawPath.endsWith(ext.ext))?.type || 'plain/text'
+    };
     response.body = body;
     return false;
 };
