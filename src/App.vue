dcec73a6ff295dcec93924b19430a78a2f046e92
zhangxin <812603872@qq.com>
2024-10-13 21:38:43 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,11 @@
 import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
 import { update } from './api';
-import { isAddListener } from './store';
+
 onMounted(() => {
   update('event', {
     event: '页面加载完成',
   });
-  if (!isAddListener.value) {
-    isAddListener.value = true;
-    document.body.addEventListener('click', (e) => {
-      const target = e.target as HTMLElement;
-      const tag = target.dataset['tag'];
-      if (!tag) return;
-      update('event', {
-        tag,
-        event: '点击',
-      });
-    });
-  }
 });
 </script>
 
